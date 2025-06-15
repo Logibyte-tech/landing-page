c5ad633afd4a625bc2928f135fac324a52d75f62
Tong Zhou <tong.zhou@logibyte.com>
2025-06-15 02:55:14 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type { import("next").NextConfig } */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-}
+  output: "export",
+  // (Optional) If you're using images (for example, from unsplash) and you're not using a custom server (or "next start"), you'll also need to set "images" to "unoptimized" (or use "images.remotePatterns" (recommended) if you're using "images.domains" (which is deprecated)):
+  images: { unoptimized: true, /* (or use "remotePatterns" if you're using "domains") */ },
+  // (Optional) If your repo is not at the root (e.g. "username.github.io/repo"), you'll need "basePath" and "assetPrefix" (for example, "basePath": "/YOUR_REPO_NAME" and "assetPrefix": "/YOUR_REPO_NAME/").
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig; 
